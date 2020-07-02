0186e731bf98e3349e6f0c9a000b339ca0b3de88
ma <ma@nexttuesday.de>
2020-07-02 20:38:14 +0200

    Get hardcoded Employees

--- src/app/employee/component/employee-list/employee-list.component.ts
+++ src/app/employee/component/employee-list/employee-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service';
 import { Employee } from '../../model/employee';
 import { Observable } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
@@ -9,12 +10,21 @@ import { Observable } from 'rxjs';
   styleUrls: ['./employee-list.component.scss']
 })
 export class EmployeeListComponent implements OnInit {
-  employees$: Observable<Employee[]>
+  employees$: Observable<Employee> | Observable<Employee[]>
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
-    this.employees$ = this.employeeService.getAll()
+    this.employees$ = this.employeeService.getAll();
+  }
+
+  detailEmployee(employee: Employee) {
+    console.log(employee)
+    this.router.navigate([employee.id], { relativeTo: this.route });
   }
 
 }
--- src/app/employee/services/employee.service.ts
+++ src/app/employee/services/employee.service.ts
@@ -2,20 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Employee } from 'src/app/employee/model/employee';
 import { config } from 'src/app/config';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
+  employees = [
+    new Employee("1","Romeo","Duerdin","rduerdin0@reddit.com","Male","6638243591"),
+    new Employee("2","Wood","Cleobury","wcleobury1@taobao.com","Male","1272716107"),
+    new Employee("3","Fidelity","Barnbrook","fbarnbrook2@rediff.com","Female","2045241026"),
+    new Employee("4","Cal","Ducker","cducker3@utexas.edu","Female","8292559046"),
+    new Employee("5","Sutton","Ozanne","sozanne4@pinterest.com","Male","1073859525"),
+  ]
+
   constructor(private http: HttpClient) { }
 
   getAll() {
-    return this.http.get<Employee[]>(`${config.apiUrl}/employees`);
+    return of(this.employees)
+    // return this.http.get<Employee[]>(`${config.apiUrl}/employees`);
   }
 
-  getById(id: number) {
-    return this.http.get(`${config.apiUrl}/employees/` + id);
+  getById(id: String) {
+    return of(this.employees[Number(id) - 1])
+    // return this.http.get(`${config.apiUrl}/employees/` + id);
   }
 
 }
