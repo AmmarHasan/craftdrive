78ef2b8d5591dae88c6bc8f022a47c3dbe7a44db
ma <ma@nexttuesday.de>
2020-07-02 20:37:57 +0200

    Add id and gender to Employee model

--- src/app/employee/model/employee.ts
+++ src/app/employee/model/employee.ts
@@ -1,8 +1,10 @@
 export class Employee {
   public constructor(
+    public id: String,
     public first_name: String,
     public last_name: String,
     public email: String,
+    public gender: String,
     public phone: String,
   ){
 
