d3347d69f8580aca36f5368c706b73edef5f632e
ma <ma@nexttuesday.de>
2020-07-02 19:19:47 +0200

    Employee model

---
+++ src/app/model/employee.ts
@@ -0,0 +1,11 @@
+export class Employee {
+  public constructor(
+    public first_name: String,
+    public last_name: String,
+    public email: String,
+    public phone: String,
+  ){
+
+  }
+
+}
